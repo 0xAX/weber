1427e83cfca71b670e1c379b92f14c3cb8d4e8e4
Darko Fabijan <darko@renderedtext.com>
2014-01-07 20:10:27 +0100

    Move new task to it's own file.

--- lib/mix/tasks/weber.ex
+++ lib/mix/tasks/weber.ex
@@ -6,7 +6,8 @@ defmodule Mix.Tasks.Weber do
 
     Usage:
 
-      mix weber.new /home/user/myWebApp - Creates myWebApp directory with weber project skeleton.
+      mix weber.new /home/user/my_web_app - Creates myWebApp directory with weber project skeleton.
+      mix weber.routes -- Prints out all defined routes.
       mix weber --version - Prints weber version.
       mix weber --help - Prints help.
   """
@@ -29,72 +30,15 @@ defmodule Mix.Tasks.Weber do
     Mix.shell.info "Weber v#{@version}"
   end
 
-  def usage do
+  defp usage do
     Mix.shell.info """
     Usage:
 
-      mix weber.new /home/user/testWebApp -- creates new weber web application
-      mix weber.routes -- prints out all defined routes
-      mix weber --version -- shows weber version
-      mix weber --help    -- shows weber help
+      mix weber.new /home/user/my_web_app - Creates myWebApp directory with weber project skeleton.
+      mix weber.routes -- Prints out all defined routes.
+      mix weber --version - Prints weber version.
+      mix weber --help - Prints help.
     """
   end
 
-  defmodule New do
-
-    @doc """
-    Create default weber based project.
-    """
-    def run([directoryName]) do
-      # create project directory
-      case File.mkdir(directoryName) do
-        :ok -> :ok
-        err -> :io.format("[Weber error] Can't create project directory. Error: ~p~n", [err])
-      end
-
-      #
-      # create project skeleton
-      #
-      path = Path.expand directoryName
-      baseName = Path.basename directoryName
-
-      vars = HashDict.new [
-        {"path", path},
-        {"projectNamespace", Mix.Utils.camelize(baseName)},
-        {"projectName", baseName}
-      ]
-
-      template = "default"
-      {:ok, pwd} = File.cwd
-      skelRoot = pwd <> "/templates/" <> template
-      File.cd skelRoot
-      skelFiles = Weber.Utils.get_all_files(".")
-      lc file inlist skelFiles do
-        baseFile = String.slice(file, 1, 1024)
-        destination = path <> replace(baseFile, vars)
-        dir = :filename.dirname(destination)
-        if !:filelib.is_dir(dir), do: Mix.Generator.create_directory dir
-        File.cp_r file, destination, fn _, _ -> true end
-        {:ok, origin} = File.read destination
-        compiled = replace(origin, vars)
-        File.write destination, compiled, []
-      end
-    end
-
-    defp replace(text, vars) do
-      case Enum.uniq(Regex.scan(%r/\#\{([^\}]+)\}/, text)) do
-        []   -> text
-        data -> replace_act(text, vars, data)
-      end
-    end
-
-    defp replace_act(text, _vars, []) do
-      text
-    end
-
-    defp replace_act(text, vars, [[entry, key] | tail]) do
-      replace_act(String.replace(text, entry, HashDict.get(vars, key), []), vars, tail)
-    end
-
-  end
 end
---
+++ lib/mix/tasks/weber/new.ex
@@ -0,0 +1,57 @@
+defmodule Mix.Tasks.Weber.New do
+
+  @doc """
+  Create default weber based project.
+  """
+  def run([directoryName]) do
+    # create project directory
+    case File.mkdir(directoryName) do
+      :ok -> :ok
+      err -> :io.format("[Weber error] Can't create project directory. Error: ~p~n", [err])
+    end
+
+    #
+    # create project skeleton
+    #
+    path = Path.expand directoryName
+    baseName = Path.basename directoryName
+
+    vars = HashDict.new [
+    {"path", path},
+    {"projectNamespace", Mix.Utils.camelize(baseName)},
+      {"projectName", baseName}
+      ]
+
+      template = "default"
+      {:ok, pwd} = File.cwd
+      skelRoot = pwd <> "/templates/" <> template
+      File.cd skelRoot
+      skelFiles = Weber.Utils.get_all_files(".")
+      lc file inlist skelFiles do
+        baseFile = String.slice(file, 1, 1024)
+        destination = path <> replace(baseFile, vars)
+        dir = :filename.dirname(destination)
+        if !:filelib.is_dir(dir), do: Mix.Generator.create_directory dir
+        File.cp_r file, destination, fn _, _ -> true end
+        {:ok, origin} = File.read destination
+        compiled = replace(origin, vars)
+        File.write destination, compiled, []
+      end
+  end
+
+  defp replace(text, vars) do
+    case Enum.uniq(Regex.scan(%r/\#\{([^\}]+)\}/, text)) do
+      []   -> text
+      data -> replace_act(text, vars, data)
+    end
+  end
+
+  defp replace_act(text, _vars, []) do
+    text
+  end
+
+  defp replace_act(text, vars, [[entry, key] | tail]) do
+    replace_act(String.replace(text, entry, HashDict.get(vars, key), []), vars, tail)
+  end
+
+end
